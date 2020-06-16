8ba21dd7b0111819b2263f666b6cd3966ed4f6ef
RebornInferno <antonpirogov@mail.ru>
2020-06-16 13:17:39 +0300

    no message

--- routes/api.js
+++ routes/api.js
@@ -31,19 +31,19 @@ router.post("api/exercise/new-user", async (req,res) =>
     }
 })
 
-//get an array of all users by getting api/exercise/users 
+//get an array of all users by getting api/exercise/users d
 //with the same info as when creating a user.
-router.get()
+//router.get()
 
 //add an exercise to any user by posting form data userId(_id),
 //description, duration, and optionally date to /api/exercise/add.
 //If no date supplied it will use current date. 
 //Returned will be the user object with also with the exercise fields added.
-router.post
+//router.post
 
 //retrieve a full exercise log of any user by getting /api/exercise/log with a parameter of userId(_id).
 //Return will be the user object with added array log and count (total exercise count).
 //retrieve part of the log of any user by also passing along optional parameters of from & to or limit.
 //(Date format yyyy-mm-dd, limit = int)
-router.get
+//router.get
 
