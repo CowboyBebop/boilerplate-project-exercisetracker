10b6d37183a261b62017007491ff90175f38c645
RebornInferno <antonpirogov@mail.ru>
2020-06-16 13:15:14 +0300

    new user post

--- models/exercises.js
+++ models/exercises.js
@@ -1,1 +1,48 @@
+const mongoose = require('mongoose');
+const { exists } = require('./users');
+const Schema = mongoose.Schema;
 
+Exercise = Schema({
+    description:{
+        type: String,
+        required: true,
+        maxlength: [30, 'description too long']
+    },
+    duration:{
+        type: Number,
+        required: true,
+        min: [1, 'duration too short']
+    },
+    date: {
+        type: Date,
+        default: Date.now
+      },
+    username: String,
+    userId: {
+        type:String,
+        ref: 'Users',
+        index: true
+    }
+})
+
+Exercises.pre('save', function(next) {
+    mongoose.model('Users').findById(this.userId, (err, user) => {
+      if(err) return next(err)
+      
+      //If there's no user in the user model, return an error
+      if(!user) {
+        const err = new Error('unknown userId')
+        err.status = 400
+        return next(err);
+      }
+      
+      //if the user exists, check it's username and assign it in the Execrise document
+      this.username = user.username
+
+      if(!this.date) {
+        this.date = Date.now();
+      }
+      next();
+    });
+  });
+  
