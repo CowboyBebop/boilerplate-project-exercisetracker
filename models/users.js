3240777c645cbccec8fe6a84ee7c99005f852013
RebornInferno <antonpirogov@mail.ru>
2020-06-17 16:18:26 +0300

    no message

--- models/users.js
+++ models/users.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Users = new Schema({
-    username: {
-      type: String, 
-      required: true,
-      unique: true,
-      maxlength: [20, 'username too long']
-    }
-  });
+  username: {
+    type: String, 
+    required: true,
+    unique: true,
+    maxlength: [20, 'username too long']
+  }
+});
   
-  module.exports = mongoose.model('Users', Users);
+module.exports = mongoose.model('Users', Users);
